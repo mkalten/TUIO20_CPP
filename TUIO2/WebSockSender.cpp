d8005c2ee726b694af8408f99216d1529bc1eaae
Martin Kaltenbrunner <modin@yuri.at>
2015-02-02 16:49:43 +0100

    included sha1 & base64 in the WebSockSender class

--- TUIO2/WebSockSender.cpp
+++ TUIO2/WebSockSender.cpp
@@ -1,6 +1,6 @@
 /*
  TUIO2 C++ Library
- Copyright (c) 2009-2014 Martin Kaltenbrunner <martin@tuio.org>
+ Copyright (c) 2009-2015 Martin Kaltenbrunner <martin@tuio.org>
  
  This library is free software; you can redistribute it and/or
  modify it under the terms of the GNU Lesser General Public
@@ -17,10 +17,6 @@
 */
 
 #include "WebSockSender.h"
-
-#include <stdio.h>
-#include "base64.h"
-#include "sha1.h"
 
 #ifdef  WIN32
 #ifndef int32_t
@@ -121,4 +117,168 @@ void WebSockSender::newClient( int tcp_client ) {
 
 	send(tcp_client,buf, strlen(buf),0);
 }
+
+
+/*
+ * Incredibly minimal implementation of SHA1.
+ * Totally independent of any other code (even libc) so it can be
+ * run on bare hardware.
+ *
+ * Copyright (C) 2009 John Stumpo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *		 * Redistributions of source code must retain the above copyright
+ *			 notice, this list of conditions and the following disclaimer.
+ *		 * Redistributions in binary form must reproduce the above copyright
+ *			 notice, this list of conditions and the following disclaimer in the
+ *			 documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY JOHN STUMPO ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL JOHN STUMPO BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+void WebSockSender::sha1( uint8_t digest[SHA1_HASH_SIZE], const uint8_t* inbuf, size_t length) {
+	
+	size_t i, j;
+	int remaining_bytes;
+	uint32_t h0, h1, h2, h3, h4, a, b, c, d, e, temp;
+	uint32_t w[80];
+	unsigned char buf[64];
+	
+	/* Initialize SHA1 hash state. */
+	h0 = 0x67452301;
+	h1 = 0xefcdab89;
+	h2 = 0x98badcfe;
+	h3 = 0x10325476;
+	h4 = 0xc3d2e1f0;
+	
+	/* The extra 9 bytes are the pad byte (0x80) and 64-bit bit count that
+	 are appended to the data being hashed.	(There will more than likely
+	 also be some zeroes in between the 0x80 and the bit count so that we
+	 operate on a multiple of 64 bytes; 9 bytes, though, is the minimal
+	 amount of extra data.)	*/
+	for (i = 0; i < length + 9; i += 64) {
+		
+		/* Perform any padding necessary. */
+		remaining_bytes = length - i;
+		if (remaining_bytes >= 64) {
+			memcpy(buf, inbuf + i, 64);
+		} else if (remaining_bytes >= 0) {
+			memcpy(buf, inbuf + i, remaining_bytes);
+			memset(buf + remaining_bytes, 0, 64 - remaining_bytes);
+			buf[remaining_bytes] = 0x80;
+		} else {
+			memset(buf, 0, 64);
+		}
+		
+		if (remaining_bytes < 56)
+			*(uint32_t*)(buf + 60) = SWAP(length * 8);
+		
+		/* Build the input array. */
+		for (j = 0; j < 16; j++)
+			w[j] = SWAP(*(uint32_t*)(buf + j * 4));
+		
+		for (j = 16; j < 80; j++)
+			w[j] = ROL(w[j - 3] ^ w[j - 8] ^ w[j - 14] ^ w[j - 16], 1);
+		
+		/* Load hash state. */
+		a = h0;
+		b = h1;
+		c = h2;
+		d = h3;
+		e = h4;
+		
+		for (j = 0; j < 80; j++) {
+			
+			if (j < 20)
+				temp = ((b & c) | ((~b) & d)) + 0x5a827999;
+			else if (j < 40)
+				temp = (b ^ c ^ d) + 0x6ed9eba1;
+			else if (j < 60)
+				temp = ((b & c) | (b & d) | (c & d)) + 0x8f1bbcdc;
+			else
+				temp = (b ^ c ^ d) + 0xca62c1d6;
+			
+			temp += ROL(a, 5) + e + w[j];
+			
+			e = d;
+			d = c;
+			c = ROR(b, 2);
+			b = a;
+			a = temp;
+		}
+		
+		/* Incorporate the results of the hash operation. */
+		h0 += a;
+		h1 += b;
+		h2 += c;
+		h3 += d;
+		h4 += e;
+	}
+	
+	/* Write the hash into the output buffer. */
+	*(uint32_t*)(digest) = SWAP(h0);
+	*(uint32_t*)(digest + 4) = SWAP(h1);
+	*(uint32_t*)(digest + 8) = SWAP(h2);
+	*(uint32_t*)(digest + 12) = SWAP(h3);
+	*(uint32_t*)(digest + 16) = SWAP(h4);
+}
+
+/* 
+ * a very simple base64 encoder, licensed as public domain. original source:
+ * https://en.wikibooks.org/wiki/Algorithm_Implementation/Miscellaneous/Base64
+ */
+
+const static unsigned char encodeLookup[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+const static unsigned char padCharacter = '=';
+
+std::string WebSockSender::base64( uint8_t* cursor, size_t size ) {
+	
+	std::string encodedString;
+	uint32_t temp;
+	
+	encodedString.reserve(((size/3) + (size % 3 > 0)) * 4);
+	
+	for (size_t idx = 0; idx < size/3; idx++) {
+		
+		temp  = (*cursor++) << 16;
+		temp += (*cursor++) << 8;
+		temp += (*cursor++);
+		
+		encodedString.append( 1, encodeLookup[(temp & 0x00FC0000) >> 18] );
+		encodedString.append( 1, encodeLookup[(temp & 0x0003F000) >> 12] );
+		encodedString.append( 1, encodeLookup[(temp & 0x00000FC0) >> 6 ] );
+		encodedString.append( 1, encodeLookup[(temp & 0x0000003F)      ] );
+	}
+	
+	switch (size % 3) {
+		case 1:
+			temp = (*cursor++) << 16;
+			encodedString.append( 1, encodeLookup[(temp & 0x00FC0000) >> 18] );
+			encodedString.append( 1, encodeLookup[(temp & 0x0003F000) >> 12] );
+			encodedString.append( 2, padCharacter );
+			break;
+		case 2:
+			temp  = (*cursor++) << 16;
+			temp += (*cursor++) << 8;
+			encodedString.append( 1, encodeLookup[(temp & 0x00FC0000) >> 18] );
+			encodedString.append( 1, encodeLookup[(temp & 0x0003F000) >> 12] );
+			encodedString.append( 1, encodeLookup[(temp & 0x00000FC0) >> 6 ] );
+			encodedString.append( 1, padCharacter);
+			break;
+	}
+	
+	return encodedString;
+}
+
 
--- TUIO2/WebSockSender.h
+++ TUIO2/WebSockSender.h
@@ -1,6 +1,7 @@
 /*
  TUIO2 C++ Library
- Copyright (c) 2009-2014 Martin Kaltenbrunner <martin@tuio.org>
+ Copyright (c) 2009-2015 Martin Kaltenbrunner <martin@tuio.org>
+ WebSockSender (c) 2015 Florian Echtler <floe@butterbrot.org>
  
  This library is free software; you can redistribute it and/or
  modify it under the terms of the GNU Lesser General Public
@@ -19,7 +20,20 @@
 #ifndef INCLUDED_WEBSOCKSENDER_H
 #define INCLUDED_WEBSOCKSENDER_H
 
+
+/* All of these macros assume use on a 32-bit variable.
+ Additionally, SWAP assumes we're little-endian.	*/
+#define SWAP(a) ((((a) >> 24) & 0x000000ff) | (((a) >>	8) & 0x0000ff00) | \
+(((a) <<	8) & 0x00ff0000) | (((a) << 24) & 0xff000000))
+#define ROL(a, b) (((a) << (b)) | ((a) >> (32 - (b))))
+#define ROR(a, b) ROL((a), (32 - (b)))
+#define SHA1_HASH_SIZE (160/8)
+
 #include "TcpSender.h"
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <vector>
 
 namespace TUIO2 {
 	
@@ -64,7 +78,11 @@ namespace TUIO2 {
 		 * @param tcp_client the socket handle of the new client
 		 */
 		void newClient( int tcp_client );
-
+	
+	private:
+		
+		void sha1( uint8_t digest[SHA1_HASH_SIZE], const uint8_t* inbuf, size_t length );
+		std::string base64( uint8_t* buffer, size_t size );
 	};
 }
 #endif /* INCLUDED_WEBSOCKSENDER_H */
